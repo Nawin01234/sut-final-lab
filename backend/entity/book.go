bb4c84b0a99221891396ad34fb92f9070546e530
Nawin01234 <1234witthawin@gmail.com>
2026-01-07 12:10:42 +0700

    dwad close #1

---
+++ backend/entity/book.go
@@ -0,0 +1,12 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+
+type Books struct {
+	gorm.Model
+	Title string  `valid:"stringlength(3|100)"`
+	Price float64 `valid:"range(50|5000)~Price must be between 50 and 5000"`
+	Code  string  `valid:"matches(BK)~Code must start with BK followed by 6 digits (0-9)"`
+}
